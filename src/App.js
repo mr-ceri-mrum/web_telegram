2e1bc8a854bb2ecbe0fd4ba5efc43ac21791e1d2
makabake9 <zhunussoffilias0216@gmail.com>
2023-03-03 16:34:30 +0600

    add the Button

--- src/App.js
+++ src/App.js
@@ -2,18 +2,25 @@ import './App.css';
 import {useEffect} from "react";
 import {useTelegram} from "./hooks/useTelegram";
 import Header from "./components/Header/Header";
-
+import {Route, Routes} from 'react-router-dom'
+import ProductList from "./components/ProductList/ProductList";
+import Form from "./components/Form/Form";
 
 const tg = window.Telegram.WebApp;
 function App() {
-  const {onClose} = useTelegram();
+    const {onClose} = useTelegram();
     useEffect(() => {
         tg.ready();
     }, []);
-  return (
+
+    return (
     <div className="App">
         <Header />
-        <button onClick={onClose}>Close</button>
+        <Routes>
+             <Route index element={<ProductList />} />
+             <Route path={'form'} element={<Form />} />
+
+        </Routes>
     </div>
   );
 }
